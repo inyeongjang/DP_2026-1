61293fd19a00000d39e5458663a1f9ed7b0550da
inyeongjang <726jang@gmail.com>
2026-03-22 02:03:35 +0900

    0319

---
+++ src/practice/ch02/delegation/PrintBannerAdapter02.java
@@ -0,0 +1,24 @@
+package practice.ch02.delegation;
+
+// 자바에서는 다중 상속이 불가능함 (부모가 둘 이상은 허용되지 않음)
+public class PrintBannerAdapter02 extends Print {
+    private Banner banner; // Banner 클래스의 인스턴스를 참조하는 필드 
+
+    public PrintBannerAdapter02(String text) {
+        this.banner = new Banner(text); // Banner 클래스의 인스턴스를 생성하여 필드에 할당 
+    }
+
+    @Override
+    public void printWeak() {
+        banner.showWithParen(); // Banner 클래스의 메서드를 호출하여 기능을 수행 (위임)
+    }
+
+    @Override
+    public void printStrong() {
+        banner.showWithAster(); // Banner 클래스의 메서드를 호출하여 기능을 수행 (위임)
+    }
+
+    public String getYourName() {
+        return "홍길동"; 
+    }
+}
