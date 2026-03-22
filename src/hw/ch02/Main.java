0ac6077264da37360f48e52df3be843d60745bc1
inyeongjang <726jang@gmail.com>
2026-03-23 01:50:30 +0900

    2장 과제 완료

---
+++ src/hw/ch02/Main.java
@@ -0,0 +1,20 @@
+package hw.ch02;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("20230918 장인영");
+
+        // 1. 한국 콘센트 생성
+        KoreanOutlet outlet = new KoreanOutlet();
+
+        // 2. Adapter 생성 (콘센트를 Charger로 변환)
+        Charger adapter = new KoreanOutletAdapter(outlet);
+
+        // 3. 스마트폰에 연결
+        Smartphone phone = new Smartphone(adapter);
+
+        // 4. 충전 실행
+        phone.charge();
+    }
+}
