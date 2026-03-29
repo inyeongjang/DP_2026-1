64f0390f6065e4a72e238ea8ec9eafea24dc313e
inyeongjang <726jang@gmail.com>
2026-03-30 03:43:04 +0900

    3장 과제 완료

--- src/hw/ch03/Main.java
+++ src/hw/ch03/Main.java
@@ -2,6 +2,8 @@ package hw.ch03;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println("학번 : 20230918, 이름 : 장인영");
+
         // [ 3 - 1 ] 기본 테스트 
         System.out.println("\n[ 3 - 1 ] 기본 테스트\n");
         AbstractDisplay d1 = new CharDisplay('H');
