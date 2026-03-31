90ac60e317852f1402688f34a1760cac8d0d953e
inyeongjang <726jang@gmail.com>
2026-03-31 10:25:46 +0900

    0331

--- src/ch05/A2b/Triple.java
+++ src/ch05/A2b/Triple.java
@@ -1,13 +1,13 @@
 package ch05.A2b;
 
 public enum Triple {
-    ALPHA, BETA, GAMMA;
+    ALPHA, BETA, GAMMA; // Triple 객체 3개가 만들어짐 
 
     private Triple() {
         System.out.println("The instance " + this + " is created.");
     }
 
     public static Triple getInstance(String name) {
-        return valueOf(name);
+        return valueOf(name); // valueOf 메소드 이용 : 상수 이름 주면 해당 객체 Return 
     }
 }
