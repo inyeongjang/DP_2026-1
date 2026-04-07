63406ffae101c8cf70b953d2f63a413f8ba8039c
inyeongjang <726jang@gmail.com>
2026-04-07 10:28:52 +0900

    0407

--- src/practice/ch06/Main.java
+++ src/practice/ch06/Main.java
@@ -1,12 +1,50 @@
 package practice.ch06;
 
+import practice.ch06.framework.Manager;
+import practice.ch06.framework.Product;
+
 public class Main {
     public static void main(String[] args) {
+
+        /*
+
+        // 원본(prototype)이 일을 함 
         MessageBox mbox1 = new MessageBox('*');
         mbox1.use("Hello, world."); 
 
+        // 복제된 객체가 일을 함 
         mbox1.createCopy().use("Hello, world.");  // 복제 
         // mbox1.clone(); // 이렇게는 할 수 없음. clone() 메소드는 자신의 클래스에서만 호출 가능 
         
+        // 원본(prototype)이 일을 함 
+        UnderlinePen upen1 = new UnderlinePen('+');
+        upen1.use("Hello, world.");
+        
+        // 복제된 객체가 일을 함 
+        upen1.createCopy().use("Hello, world.");
+
+        */
+
+        // Manager를 이용하는 코드 
+
+        // 준비 
+        Manager manager = new Manager(); 
+        MessageBox mbox1 = new MessageBox('*');
+        MessageBox mbox2 = new MessageBox('/');
+        UnderlinePen upen1 = new UnderlinePen('+');
+
+        // 등록 
+        manager.register("star box", mbox1);
+        manager.register("slash box", mbox2);
+        manager.register("plus line", upen1);
+
+        // 등록된 이름으로 복제품을 얻어서 사용 
+        Product p1 = manager.create("star box");
+        p1.use("Hello, world.");
+        Product p2 = manager.create("slash box");
+        p2.use("Hello, world.");
+        Product p3 = manager.create("plus line");
+        p3.use("Hello, world.");
+
     }   
 }
