abc00ed0994fca2f698b5bd04bd00d638f32178e
inyeongjang <726jang@gmail.com>
2026-04-16 11:45:43 +0900

    0416

--- src/ch10/Q4/Main.java
+++ src/ch10/Q4/Main.java
@@ -6,12 +6,30 @@ class Main {
     public static void main(String[] args) {
         List<String> list = Arrays.asList("D", "B", "C", "E", "A");
 
-        // 사전순으로 작은 순서
-        list.sort( /* 미구현 */ );
+        // 사전순으로 작은 순서 (오름차순)
+        list.sort(new Comparator<String>() {
+            @Override
+            public int compare(String s1, String s2) {
+                // s1과 s2를 비교 
+                return s1.compareTo(s2); // s1이 더 클 때 양수가 리턴되고, 위치가 바뀌게 됨 => 큰 게 뒤로 가게 됨 => 오름차순 
+            }
+        }); // Comparator 인터페이스를 구현한 객체를 넣어주어야 함 
         System.out.println(list);
 
-        // 사전순으로 큰 순서 
-        list.sort( /* 미구현 */ );
+        list.sort((s1, s2) -> s1.compareTo(s2)); // 람다식 
+        System.out.println(list);
+
+        list.sort((s1, s2) -> s2.compareTo(s1));
+        System.out.println(list);
+
+        // 사전순으로 큰 순서 (내림차순)
+        list.sort(new Comparator<String>() {
+            @Override
+            public int compare(String s1, String s2) {
+                // s1과 s2를 비교
+                return s2.compareTo(s1); // s2가 더 클 때 양수가 리턴되고, 위치가 바뀌게 됨 => 큰 게 앞으로 가게 됨 => 내림차순
+            }
+        });
         System.out.println(list);
     }
 }
--- src/ch11/Sample/Main.java
+++ src/ch11/Sample/Main.java
@@ -7,11 +7,14 @@ public class Main {
         Directory bindir = new Directory("bin");
         Directory tmpdir = new Directory("tmp");
         Directory usrdir = new Directory("usr");
+
         rootdir.add(bindir);
         rootdir.add(tmpdir);
         rootdir.add(usrdir);
+
         bindir.add(new File("vi", 10000));
         bindir.add(new File("latex", 20000));
+        
         rootdir.printList();
         System.out.println();
 
@@ -19,14 +22,19 @@ public class Main {
         Directory youngjin = new Directory("youngjin");
         Directory gildong = new Directory("gildong");
         Directory dojun = new Directory("dojun");
+        
         usrdir.add(youngjin);
         usrdir.add(gildong);
         usrdir.add(dojun);
+        
         youngjin.add(new File("diary.html", 100));
         youngjin.add(new File("Composite.java", 200));
+
         gildong.add(new File("memo.tex", 300));
+        
         dojun.add(new File("game.doc", 400));
         dojun.add(new File("junk.mail", 500));
+        
         rootdir.printList();
     }
 }
