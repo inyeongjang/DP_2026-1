253b02eeeb06464a0cd4a22a666f73c596fea515
inyeongjang <726jang@gmail.com>
2026-04-21 09:47:48 +0900

    0421

--- src/practice/ch11/Main.java
+++ src/practice/ch11/Main.java
@@ -1,14 +1,23 @@
 package practice.ch11;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) {        
+        
         // 디렉터리 트리를 만든다. 
         Directory root = new Directory("root");
+       
+        // 업 캐스팅은 자동으로 일어난다. 
+        // Entry root = new Directory("root"); // add() 메소드 호출 시 컴파일 에러 발생 (Directory 타입의 add() 메소드는 Entry 타입에서 선언되지 않았기 때문) -> 형변환 필요 
+
         Directory bin = new Directory("bin");
         Directory tmp = new Directory("tmp");
         Directory usr = new Directory("usr");
 
         // 파일 생성 및 추가 
+        
+        // 다운 캐스팅
+        // ((Directory)root).add(bin); // 형 변환, . 연산자가 타입 캐스팅 연산자보다 우선 순위가 높다. 
+        
         root.add(bin);
         root.add(tmp);
         root.add(usr);
