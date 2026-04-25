57887fc31915c8773d587648516aa6b07f19f89c
inyeongjang <726jang@gmail.com>
2026-04-26 00:14:25 +0900

    10장 과제 완료

--- src/hw/ch10/Main.java
+++ src/hw/ch10/Main.java
@@ -13,7 +13,7 @@ public class Main {
         // CyclicStrategy 테스트: 순환 확인
         System.out.println("\n===== CyclicStrategy 의 손가락 순환 =====\n");
         Strategy cyclic = new CyclicStrategy();
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < 9; i++) {
             Hand hand = cyclic.nextHand();
             System.out.println((i+1) + "번째: " + hand);
             cyclic.study(false); // study 호출해도 영향 없음
