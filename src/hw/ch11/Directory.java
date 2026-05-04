2aa3e0f5efaffab351fe0f9ba30669cfb3863f0a
inyeongjang <726jang@gmail.com>
2026-05-04 14:07:44 +0900

    11장 과제 완료

---
+++ src/hw/ch11/Directory.java
@@ -0,0 +1,50 @@
+package hw.ch11;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Directory extends Entry {
+    private String name;
+    private List<Entry> directory = new ArrayList<>();
+
+    public Directory(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public List<Entry> search(String keyword) {
+        List<Entry> results = new ArrayList<>();
+        for (Entry entry : directory) {
+            results.addAll(entry.search(keyword));
+        }
+        return results;
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public int getSize() {
+        int size = 0;
+        for (Entry entry: directory) {
+            size += entry.getSize();
+        }
+        return size;
+    }
+
+    @Override
+    protected void printList(String prefix) {
+        System.out.println(prefix + "/" + this);
+        for (Entry entry: directory) {
+            entry.printList(prefix + "/" + name);
+        }
+    }
+
+    public Entry add(Entry entry) {
+        directory.add(entry);
+        entry.setParent(this); // 자식 엔트리에게 부모(현재 디렉토리)를 설정한다. 
+        return this;
+    }
+}
---
+++ src/hw/ch11/File.java
@@ -0,0 +1,38 @@
+package hw.ch11;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class File extends Entry {
+    private String name;
+    private int size;
+
+    public File(String name, int size) {
+        this.name = name;
+        this.size = size;
+    }
+
+    @Override
+    public List<Entry> search(String keyword) {
+        List<Entry> results = new ArrayList<>();
+        if (getName().contains(keyword)) {
+            results.add(this);      
+        }
+        return results;
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public int getSize() {
+        return size;
+    }
+
+    @Override
+    protected void printList(String prefix) {
+        System.out.println(prefix + "/" + this);
+    }
+}
