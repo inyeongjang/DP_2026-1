ce0fb0307d142c9da71e3c62429aef29567013e1
inyeongjang <726jang@gmail.com>
2026-05-13 20:07:59 +0900

    16장 과제 완료

--- src/hw/ch16/Main.java
+++ src/hw/ch16/Main.java
@@ -2,6 +2,7 @@ package hw.ch16;
 
 public class Main {
     static public void main(String[] args) {
+        System.out.println("\n학번 : 20230918, 이름 :장인영\n");
         new LoginFrame("Mediator Sample");
     }
 }
