d6901bac0b204a956c4635854044d50cacecf8e0
inyeongjang <726jang@gmail.com>
2026-05-13 20:07:43 +0900

    17장 과제 완료

--- src/hw/ch17/Main.java
+++ src/hw/ch17/Main.java
@@ -2,13 +2,18 @@ package hw.ch17;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println("\n학번 : 20230918, 이름 :장인영\n");
+        
         NumberGenerator generator = new EvenNumberGenerator(0, 30);
+        
         Observer observer1 = new DigitObserver();
         Observer observer2 = new GraphObserver();
         Observer observer3 = new RangeObserver();
+        
         generator.addObserver(observer1);
         generator.addObserver(observer2);
         generator.addObserver(observer3);
+        
         generator.execute();
     }
 }
