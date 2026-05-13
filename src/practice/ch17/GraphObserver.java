a2506aaf1998d25c11ddf39e5d806779635df066
inyeongjang <726jang@gmail.com>
2026-05-13 19:24:35 +0900

    0512

---
+++ src/practice/ch17/GraphObserver.java
@@ -0,0 +1,19 @@
+package practice.ch17;
+
+public class GraphObserver implements Observer {
+    @Override
+    public void update(NumberGenerator generator) { // 통지받을 때 호출되는 메소드
+        System.out.print("GraphObserver:");
+        int count = generator.getNumber();
+
+        for (int i = 0; i < count; i++) {
+            System.out.print("*");
+        }
+        System.out.println("");
+        
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+        }
+    }
+}
