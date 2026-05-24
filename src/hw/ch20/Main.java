3cad12517f33069017778fbc7e42ba16e93a6ec5
inyeongjang <726jang@gmail.com>
2026-05-25 03:21:54 +0900

    20장 과제 완료

---
+++ src/hw/ch20/Main.java
@@ -0,0 +1,30 @@
+package hw.ch20;
+
+public class Main {
+    static final String RED = "\u001B[31m";
+    static final String GREEN = "\u001B[32m";
+    static final String YELLOW = "\u001B[33m";
+    static final String BLUE = "\u001B[34m";
+    static final String MAGENTA = "\u001B[35m";
+    static final String CYAN = "\u001B[36m";
+    static final String RESET = "\u001B[0m";
+
+    public static void main(String[] args) {
+        System.out.println("\n학번 : 20230918, 이름 : 장인영\n");
+
+        if (args.length == 0) {
+            System.out.println("Usage: java Main digits");
+            System.out.println("Example: java Main 1212123");
+            System.exit(0);
+        }
+
+        BigString bs = new BigString(args[0]);
+
+        String[] colors = {
+            RED, BLUE, GREEN, MAGENTA, CYAN, YELLOW, BLUE
+        };
+
+        bs.print(colors);
+
+    }
+}
