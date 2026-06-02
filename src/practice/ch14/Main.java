5747ec62fc2372ef7c8e9169535e6e15b563cdec
inyeongjang <726jang@gmail.com>
2026-06-02 09:50:31 +0900

    0602

---
+++ src/practice/ch14/Main.java
@@ -0,0 +1,23 @@
+package practice.ch14;
+
+public class Main {
+    public static void main(String[] args) {
+        // 해결자들 생성 
+        Support alice = new NoSupport("Alice");
+        Support bob = new LimitSupport("Bob", 100);
+        Support charlie = new SpecialSupport("Charlie", 429);
+        Support diana = new OddSupport("Diana");
+
+        // 사슬 형성 
+        alice.setNext(bob).setNext(charlie).setNext(diana);
+
+        // 문제를 첫번째 객체에게 던져줌 
+        // alice.support(new Trouble(429));
+        // alice.support(new Trouble(99));
+        // alice.support(new Trouble(199));
+        // alice.support(new Trouble(500));
+        for (int i = 0; i < 500; i+= 33) {
+            alice.support(new Trouble(i));
+        }
+    }
+}
